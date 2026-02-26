9559125d54e1108731586c99d350165b2e6bd570
deeksha <deeksha.elkal@gmail.com>
2026-02-26 12:22:58 +0530

    day3

---
+++ mm.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class mm {
+    public static void main(String[] args) {
+        String S1="deeksha";
+        String S2="deeksha";
+        String S3 = new String("deekshaaaaaaa");
+        String S4 = new String("deekshaaaaaaa");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(S1.equals(S2));
+        
+        String str = sc.nextLine();
+        System.out.println(str.trim());
+    }
+    
+}
